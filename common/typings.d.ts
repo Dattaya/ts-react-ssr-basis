9adbdad1511c9aad73b28084b3431093163675cd
Yaroslav Kiliba <om.dattaya@gmail.com>
2019-05-16 10:41:46 +0300

    config-chore: resolve json

--- common/typings.d.ts
+++ common/typings.d.ts
@@ -48,11 +48,6 @@ declare module "*.woff2" {
   export default value
 }
 
-declare module "*.json" {
-  const value: string
-  export default value
-}
-
 declare module "*.ico" {
   const value: string
   export default value
