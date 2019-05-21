e86d10f188765badd2d43ae424898cb01e71e3d1
Yaroslav Kiliba <om.dattaya@gmail.com>
2019-05-21 11:34:31 +0300

    all-feat: add hooks lint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,10 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
-    "plugin:jsx-a11y/recommended",
+    'plugin:jsx-a11y/recommended',
+  ],
+  plugins: [
+    'react-hooks',
   ],
   parserOptions: {
     ecmaVersion: 2018,
@@ -24,10 +27,12 @@ module.exports = {
   rules: {
     'semi': ['error', 'never'],
     'react/prop-types': 'off', // TS is used instead of prop types
-    "react/no-unescaped-entities": "off",
+    'react/no-unescaped-entities': 'off',
     '@typescript-eslint/indent': ['error', 2],
-    "@typescript-eslint/explicit-function-return-type": "off", // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/explicit-function-return-type.md
-    "@typescript-eslint/no-use-before-define": "off", // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-use-before-define.md
-    "@typescript-eslint/no-explicit-any": "off", // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-explicit-any.md
+    '@typescript-eslint/explicit-function-return-type': 'off', // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/explicit-function-return-type.md
+    '@typescript-eslint/no-use-before-define': 'off', // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-use-before-define.md
+    '@typescript-eslint/no-explicit-any': 'off', // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-explicit-any.md
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn' // Checks effect dependencies
   },
 }
