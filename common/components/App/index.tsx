a007d037cb68ed862a62be878679d6b8d7c5048c
Yaroslav Kiliba <om.dattaya@gmail.com>
2019-12-03 15:43:48 +0200

    common-chore: make `App` a func component

--- common/components/App/index.tsx
+++ common/components/App/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 import { Helmet } from "react-helmet"
 
 import LazyLoadable from 'components/Lazy/loadable'
@@ -6,32 +6,24 @@ import ErrorBoundary from './ErrorBoundary'
 import './global.css?global'
 import classes from './styles.css'
 
-// you can create your own wrapper with try and catch and use it whenewer you need to lazy load a component
-class App extends React.Component {
-  public state = {
-    isLoadableComponentShown: false,
-  }
+const App: React.FunctionComponent = () => {
+  const [isLoadableComponentShown, setIsLoadableComponentShown] = useState(false)
+  const showLoadableComponent = useCallback(() => setIsLoadableComponentShown(true), [])
 
-  private showLoadableComponent = () => this.setState({ isLoadableComponentShown: true })
-
-  public render() {
-    const { isLoadableComponentShown } = this.state
-
-    return (
-      <div className="test">
-        <Helmet
-          titleTemplate="%s - ts-react-ssr-basis"
-        >
-          <title>Home</title>
-        </Helmet>
-        <h1 className={classes.title}>Welcome to Bob's Home Page</h1>
-        }
-        <ErrorBoundary>
-          {isLoadableComponentShown ? <LazyLoadable /> : <button className={classes.button} onClick={this.showLoadableComponent}>Load lazy component</button>}
-        </ErrorBoundary>
-      </div>
-    )
-  }
+  return (
+    <div className="test">
+      <Helmet
+        titleTemplate="%s - ts-react-ssr-basis"
+      >
+        <title>Home</title>
+      </Helmet>
+      <h1 className={classes.title}>Welcome to Bob's Home Page</h1>
+      }
+      <ErrorBoundary>
+        {isLoadableComponentShown ? <LazyLoadable /> : <button className={classes.button} onClick={showLoadableComponent}>Load lazy component</button>}
+      </ErrorBoundary>
+    </div>
+  )
 }
 
 export default App
--- common/components/App/tests/index.test.tsx
+++ common/components/App/tests/index.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import TestRenderer from 'react-test-renderer'
+import tr from 'react-test-renderer'
 
 import App from 'components/App'
 import LazyLoadable from 'components/Lazy/loadable'
@@ -8,16 +8,16 @@ jest.mock('components/Lazy/loadable', () => jest.fn(() => null))
 
 describe('<App />', () => {
   it('hides the button and loads the Lazy component', () => {
-    const app = TestRenderer.create(<App />)
-    app.root.findByType('button').props.onClick()
+    const app = tr.create(<App />)
+    tr.act(() => app.root.findByType('button').props.onClick())
     expect(app.root.findAllByType('button').length).toBe(0)
     expect(app.root.findAllByType(LazyLoadable).length).toBe(1)
   })
 
   it('handles errors when the Lazy component fails to load', () => {
     (LazyLoadable as unknown as jest.Mock).mockImplementationOnce(() => { throw new Error() })
-    const app = TestRenderer.create(<App />)
-    app.root.findByType('button').props.onClick()
+    const app = tr.create(<App />)
+    tr.act(() => app.root.findByType('button').props.onClick())
     expect(app.root.findAllByType(LazyLoadable).length).toBe(0)
   })
 })
